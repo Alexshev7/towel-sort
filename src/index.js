87b138ce2cb29658084b51bc79db1e111e99e451
Alexshev7 <alexwebshev@gmail.com>
2022-02-23 16:27:08 +0300

    feat: added js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (typeof matrix === 'undefined') {
+    return []; //вернуть пустой массив если тип введённой массива(матрицы) - undefined
+  }
+
+  let result = []; // пустой массив
+  // цикл для прохождения по массиву, содержащему подмассивы
+  for (let i = 0; i < matrix.length; i++) {
+    // проверка на чётность
+    if (i % 2 === 0) {
+      result = [...result, ...matrix[i]]; // если подмассив в главном массиве чётный, то складываем c предыдущими подмассивами в массив result
+    } else {
+      result = [...result, ...matrix[i].reverse()]; // если подмассив в главном массиве нечётный, то переворачиваем его и складываем с предыдущими подмассивами в массив result
+    }
+  }
+  return result; // возвращаем массив result
 }
